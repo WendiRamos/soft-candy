e04e31631f9c01bc71ac24b41592f7c8e7aaae8b
WendiRamos <wendiramos12@gmail.com>
2021-08-04 22:16:42 -0300

    Model Produto

---
+++ SoftCandy/Models/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftCandy.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Cod_Produto { get; set; }
+        public string Nome_Produto { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Preco_Venda { get; set; }
+        public string Descricao { get; set; }
+
+        public Produto(int cod_Produto, string nome_Produto, int quantidade, decimal preco_Venda, string descricao)
+        {
+            Cod_Produto = cod_Produto;
+            Nome_Produto = nome_Produto;
+            Quantidade = quantidade;
+            Preco_Venda = preco_Venda;
+            Descricao = descricao;
+        }
+
+        public Produto()
+        {
+        }
+    }
+
+}
