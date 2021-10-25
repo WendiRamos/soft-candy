75d1f7c2d593d81d5e2b4b54ca086a74ca6edad4
WendiRamos <wendiramos12@gmail.com>
2021-10-25 20:13:53 -0300

    nova migration cliente

--- SoftCandy/Migrations/20211025231004_Cliente.Designer.cs
+++ SoftCandy/Migrations/20211025231004_Cliente.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SoftCandy.Data;
 
 namespace SoftCandy.Migrations
 {
     [DbContext(typeof(SoftCandyContext))]
-    partial class SoftCandyContextModelSnapshot : ModelSnapshot
+    [Migration("20211025231004_Cliente")]
+    partial class Cliente
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
