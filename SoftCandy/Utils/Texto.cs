06c4bb8c5364b9ac1269ed589fff4da18f90e279
WendiRamos <wendiramos12@gmail.com>
2022-03-24 19:26:54 -0300

    Adicionado utils

---
+++ SoftCandy/Utils/Texto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftCandy.Utils
+{
+    public static class Texto
+    {
+        public static bool CaseInsensitiveContains(string text, string value)
+        {
+            byte[] tmp = Encoding.GetEncoding("ISO-8859-8").GetBytes(value);
+            string pesquisa = Encoding.UTF8.GetString(tmp);
+            return text.IndexOf(pesquisa, StringComparison.CurrentCultureIgnoreCase) >= 0;
+        }
+
+    }
+}
