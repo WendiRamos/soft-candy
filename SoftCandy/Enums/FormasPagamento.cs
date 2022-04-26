b693932e58a7ea0af62ffc183214914693a1b058
WendiRamos <wendiramos12@gmail.com>
2022-04-26 19:20:33 -0300

    Adicionando enum Formas de Pagamento

---
+++ SoftCandy/Enums/FormasPagamento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftCandy.Enums
+{
+    public enum FormasPagamento
+    {
+        [Display(Name = "Dinheiro")]
+        DINHEIRO = 1,
+        [Display(Name = "Cartão de Débito")]
+        CARTAO_DEBITO = 2,
+        [Display(Name = "Cartão de Crédito")]
+        CARTAO_CREDITO = 3,
+        [Display(Name = "Pix")]
+        PIX = 4
+    }
+}
