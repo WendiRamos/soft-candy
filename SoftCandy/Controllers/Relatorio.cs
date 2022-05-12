91ceb2bf023f987bcb28a17554c7278ee4958822
WendiRamos <wendiramos12@gmail.com>
2022-05-12 20:30:48 -0300

    Create Relatorio.cs

---
+++ SoftCandy/Controllers/Relatorio.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftCandy.Controllers
+{
+    public class Relatorio : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
