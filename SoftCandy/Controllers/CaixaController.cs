b21643108886bc3252339bdd7842118cd3d45cc3
WendiRamos <wendiramos12@gmail.com>
2022-06-20 15:38:59 -0300

    Update CaixaController.cs

--- SoftCandy/Controllers/CaixaController.cs
+++ SoftCandy/Controllers/CaixaController.cs
@@ -25,7 +25,7 @@ namespace SoftCandy.Controllers
         // GET: Histórico
         public async Task<IActionResult> Historico()
         {
-            if (LoginAtual.IsVendedor(User) || LoginAtual.IsAdministrador(User))
+            if (LoginAtual.IsCaixa(User) || LoginAtual.IsAdministrador(User))
             {
                 return View(await _context.Caixa
                 .Where(c => !c.EstaAberto)
@@ -35,74 +35,159 @@ namespace SoftCandy.Controllers
                 .Take(20)
                 .ToListAsync());
             }
-            return RedirectToAction("User", "Home");
+            return RedirectToAction("Login", "Funcionario");
         }
 
 
         // GET: Caixa
         public async Task<IActionResult> Caixa()
         {
-
-            if (CaixaUtils.IsAberto(_context))
+            if (LoginAtual.IsCaixa(User) || LoginAtual.IsAdministrador(User))
             {
-                return View( await _context.Caixa
-                .Where(c => c.EstaAberto)
-                .Include(c => c.Operacoes)
-                .ThenInclude(o => o.Funcionario)
-                .Include(c=> c.Comandas)
-                .Include(c => c.Deliveries)
-                .ThenInclude(d => d.Motoboy)
-                .FirstAsync());
+                if (CaixaUtils.IsAberto(_context))
+                {
+                    return View(await _context.Caixa
+                    .Where(c => c.EstaAberto)
+                    .Include(c => c.Operacoes)
+                    .ThenInclude(o => o.Funcionario)
+                    .Include(c => c.Comandas)
+                    .Include(c => c.Deliveries)
+                    .ThenInclude(d => d.Motoboy)
+                    .FirstAsync());
+                }
+                else
+                {
+                    return RedirectToAction(nameof(Abertura));
+                }
             }
-            else
-            {
-                return RedirectToAction(nameof(Abertura));
-            }
+            return RedirectToAction("Login", "Funcionario");
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Caixa([Bind("ValorAbertura")] Caixa caixa)
         {
-            return View(caixa);
+            if (LoginAtual.IsCaixa(User) || LoginAtual.IsAdministrador(User))
+            {
+                return View(caixa);
+            }
+            return RedirectToAction("Login", "Funcionario");
         }
 
         public IActionResult Abertura()
         {
-            if (CaixaUtils.IsFechado(_context))
+            if (LoginAtual.IsCaixa(User) || LoginAtual.IsAdministrador(User))
             {
-                return View();
+                if (CaixaUtils.IsFechado(_context))
+                {
+                    return View();
+                }
+                else
+                {
+                    return RedirectToAction(nameof(Caixa));
+                }
             }
-            else
-            {
-                return RedirectToAction(nameof(Caixa));
-            }
+            return RedirectToAction("Login", "Funcionario");
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Abertura([Bind("ValorDinheiroAbertura")] Caixa caixa)
         {
-            if (ModelState.IsValid)
+            if (LoginAtual.IsCaixa(User) || LoginAtual.IsAdministrador(User))
             {
-                caixa.ValorTotalFechamentoDinheiro = caixa.ValorDinheiroAbertura;
-                caixa.EstaAberto = true;
-                caixa.DataHoraAbertura = DateTime.Now;
-                caixa.FuncionarioAberturaId = LoginAtual.Id(User);
-                caixa.FuncionarioFechamentoId = LoginAtual.Id(User);
-                _context.Add(caixa);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Caixa));
+                if (ModelState.IsValid)
+                {
+                    caixa.ValorTotalFechamentoDinheiro = caixa.ValorDinheiroAbertura;
+                    caixa.EstaAberto = true;
+                    caixa.DataHoraAbertura = DateTime.Now;
+                    caixa.FuncionarioAberturaId = LoginAtual.Id(User);
+                    caixa.FuncionarioFechamentoId = LoginAtual.Id(User);
+                    _context.Add(caixa);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Caixa));
+                }
+                return View(caixa);
             }
-            return View(caixa);
+            return RedirectToAction("Login", "Funcionario");
         }
 
         // GET: Caixa/Fechamento
         public IActionResult Fechamento()
         {
-            if (CaixaUtils.IsAberto(_context))
+            if (LoginAtual.IsCaixa(User) || LoginAtual.IsAdministrador(User))
+            {
+                if (CaixaUtils.IsAberto(_context))
+                {
+                    Caixa caixa = CaixaUtils.CaixaAberto(_context);
+                    _context.Entry(caixa).Collection(c => c.Comandas).Load();
+                    _context.Entry(caixa).Collection(c => c.Deliveries).Load();
+
+                    ViewData["ComandaDinheiro"] = caixa.Comandas.Where(c => c.FormaPagamentoIsDinheiro()).Select(c => c.ValorTotal).Sum();
+                    ViewData["ComandaCredito"] = caixa.Comandas.Where(c => c.FormaPagamentoIsCredito()).Select(c => c.ValorTotal).Sum();
+                    ViewData["ComandaDebito"] = caixa.Comandas.Where(c => c.FormaPagamentoIsDebito()).Select(c => c.ValorTotal).Sum();
+                    ViewData["ComandaPix"] = caixa.Comandas.Where(c => c.FormaPagamentoIsPix()).Select(c => c.ValorTotal).Sum();
+                    ViewData["ComandaTotal"] = caixa.Comandas.Select(c => c.ValorTotal).Sum();
+
+                    ViewData["DeliveryDinheiro"] = caixa.Deliveries.Where(c => c.FormaPagamentoIsDinheiro()).Select(c => c.ValorTotal).Sum();
+                    ViewData["DeliveryCredito"] = caixa.Deliveries.Where(c => c.FormaPagamentoIsCredito()).Select(c => c.ValorTotal).Sum();
+                    ViewData["DeliveryDebito"] = caixa.Deliveries.Where(c => c.FormaPagamentoIsDebito()).Select(c => c.ValorTotal).Sum();
+                    ViewData["DeliveryPix"] = caixa.Deliveries.Where(c => c.FormaPagamentoIsPix()).Select(c => c.ValorTotal).Sum();
+                    ViewData["DeliveryTotal"] = caixa.Deliveries.Select(c => c.ValorTotal).Sum();
+
+                    return View(caixa);
+                }
+                else
+                {
+                    return RedirectToAction(nameof(Historico));
+                }
+            }
+            return RedirectToAction("Login", "Funcionario");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> FechamentoCaixa()
+        {
+            if (LoginAtual.IsCaixa(User) || LoginAtual.IsAdministrador(User))
             {
                 Caixa caixa = CaixaUtils.CaixaAberto(_context);
+                _context.Entry(caixa).Collection(c => c.Comandas).Load();
+                _context.Entry(caixa).Collection(c => c.Deliveries).Load();
+
+                if (caixa.ExisteVendaPendente() && !LoginAtual.IsAdministrador(User))
+                {
+                    return RedirectToAction(nameof(Error), new { message = "Existe pedido sem receber!" });
+                }
+
+                caixa.EstaAberto = false;
+                caixa.FuncionarioFechamentoId = LoginAtual.Id(User);
+                caixa.DataHoraFechamento = DateTime.Now;
+                _context.Update(caixa);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Historico));
+            }
+            return RedirectToAction("Login", "Funcionario");
+        }
+
+        // GET: Caixa/Details/
+        public async Task<IActionResult> Details(int id)
+        {
+            if (LoginAtual.IsCaixa(User) || LoginAtual.IsAdministrador(User))
+            {
+                var caixa = await _context.Caixa
+                .Include(c => c.Comandas)
+                .Include(c => c.Operacoes)
+                .Include(c => c.FuncionarioAbertura)
+                .Include(c => c.FuncionarioFechamento)
+                .Include(c => c.Deliveries)
+                .ThenInclude(d => d.Motoboy)
+                .FirstOrDefaultAsync(m => m.IdCaixa == id);
+                if (caixa == null)
+                {
+                    return NotFound();
+                }
                 _context.Entry(caixa).Collection(c => c.Comandas).Load();
                 _context.Entry(caixa).Collection(c => c.Deliveries).Load();
 
@@ -117,67 +202,9 @@ namespace SoftCandy.Controllers
                 ViewData["DeliveryDebito"] = caixa.Deliveries.Where(c => c.FormaPagamentoIsDebito()).Select(c => c.ValorTotal).Sum();
                 ViewData["DeliveryPix"] = caixa.Deliveries.Where(c => c.FormaPagamentoIsPix()).Select(c => c.ValorTotal).Sum();
                 ViewData["DeliveryTotal"] = caixa.Deliveries.Select(c => c.ValorTotal).Sum();
-
                 return View(caixa);
             }
-            else
-            {
-                return RedirectToAction(nameof(Historico));
-            }
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> FechamentoCaixa()
-        {
-            Caixa caixa = CaixaUtils.CaixaAberto(_context);
-            _context.Entry(caixa).Collection(c => c.Comandas).Load();
-            _context.Entry(caixa).Collection(c => c.Deliveries).Load();
-
-            if (caixa.ExisteVendaPendente() && !LoginAtual.IsAdministrador(User))
-            {
-                 return RedirectToAction(nameof(Error), new { message = "Existe pedido sem receber!" });
-            }
-
-            caixa.EstaAberto = false;
-            caixa.FuncionarioFechamentoId = LoginAtual.Id(User);
-            caixa.DataHoraFechamento = DateTime.Now;
-            _context.Update(caixa);
-            await _context.SaveChangesAsync();
-
-            return RedirectToAction(nameof(Historico));
-        }
-
-        // GET: Caixa/Details/
-        public async Task<IActionResult> Details(int id)
-        {
-            var caixa = await _context.Caixa
-                .Include(c => c.Comandas)
-                .Include(c => c.Operacoes)
-                .Include(c => c.FuncionarioAbertura)
-                .Include(c => c.FuncionarioFechamento)
-                .Include(c => c.Deliveries)
-                .ThenInclude(d => d.Motoboy)
-                .FirstOrDefaultAsync(m => m.IdCaixa == id);
-            if (caixa == null)
-            {
-                return NotFound();
-            }
-            _context.Entry(caixa).Collection(c => c.Comandas).Load();
-            _context.Entry(caixa).Collection(c => c.Deliveries).Load();
-
-            ViewData["ComandaDinheiro"] = caixa.Comandas.Where(c => c.FormaPagamentoIsDinheiro()).Select(c => c.ValorTotal).Sum();
-            ViewData["ComandaCredito"] = caixa.Comandas.Where(c => c.FormaPagamentoIsCredito()).Select(c => c.ValorTotal).Sum();
-            ViewData["ComandaDebito"] = caixa.Comandas.Where(c => c.FormaPagamentoIsDebito()).Select(c => c.ValorTotal).Sum();
-            ViewData["ComandaPix"] = caixa.Comandas.Where(c => c.FormaPagamentoIsPix()).Select(c => c.ValorTotal).Sum();
-            ViewData["ComandaTotal"] = caixa.Comandas.Select(c => c.ValorTotal).Sum();
-
-            ViewData["DeliveryDinheiro"] = caixa.Deliveries.Where(c => c.FormaPagamentoIsDinheiro()).Select(c => c.ValorTotal).Sum();
-            ViewData["DeliveryCredito"] = caixa.Deliveries.Where(c => c.FormaPagamentoIsCredito()).Select(c => c.ValorTotal).Sum();
-            ViewData["DeliveryDebito"] = caixa.Deliveries.Where(c => c.FormaPagamentoIsDebito()).Select(c => c.ValorTotal).Sum();
-            ViewData["DeliveryPix"] = caixa.Deliveries.Where(c => c.FormaPagamentoIsPix()).Select(c => c.ValorTotal).Sum();
-            ViewData["DeliveryTotal"] = caixa.Deliveries.Select(c => c.ValorTotal).Sum();
-            return View(caixa);
+            return RedirectToAction("Login", "Funcionario");
         }
 
         public IActionResult Error(string message)
