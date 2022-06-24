fd9ec2b53c8fdb051921a125e69b77f779e2b7d0
WendiRamos <wendiramos12@gmail.com>
2022-06-23 22:24:21 -0300

    Corrigindo os enums

--- SoftCandy/Enums/MedidaEnum.cs
+++ SoftCandy/Enums/MedidaEnum.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,10 @@ namespace SoftCandy.Enums
 {
     public enum MedidaEnum
     {
+        [Display(Name = "Unidade")]
         UNIDADE = 1,
 
+        [Display(Name = "Ml")]
         ML = 2
     }
 }
