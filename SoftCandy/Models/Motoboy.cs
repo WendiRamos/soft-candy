675bee667627ccee9a74635727f44cec660a5285
WendiRamos <wendiramos12@gmail.com>
2022-06-25 20:40:10 -0300

    Validação Motoboy - part1

--- SoftCandy/Models/Motoboy.cs
+++ SoftCandy/Models/Motoboy.cs
@@ -12,24 +12,37 @@ namespace SoftCandy.Models
         [Display(Name = "Id")]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O tamanho do {0} deve estar entre {2} e {1}.")]
         [Display(Name = "Nome")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
         [Display(Name = "Celular")]
         public string Celular { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(60, MinimumLength = 10, ErrorMessage = "O tamanho do {0} deve estar entre {2} e {1}.")]
         [Display(Name = "Logradouro")]
         public string Logradouro { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(4, MinimumLength = 2, ErrorMessage = "O tamanho do {0} deve estar entre {2} e {1}.")]
         [Display(Name = "Número")]
         public string Numero { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(60, MinimumLength = 10, ErrorMessage = "O tamanho do {0} deve estar entre {2} e {1}.")]
         [Display(Name = "Bairro")]
         public string Bairro { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(60, MinimumLength = 5, ErrorMessage = "O tamanho do {0} deve estar entre {2} e {1}.")]
         [Display(Name = "Cidade")]
         public string Cidade { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "O tamanho do {0} deve estar entre {2} e {1}.")]
         [Display(Name = "Estado")]
         public string Estado { get; set; }
 
