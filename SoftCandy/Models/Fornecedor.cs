80018b8cfe8efdcda81385ecf2a32a808ed45bc8
WendiRamos <wendiramos12@gmail.com>
2022-06-26 15:02:41 -0300

    Validação - Fornecedor

--- SoftCandy/Models/Fornecedor.cs
+++ SoftCandy/Models/Fornecedor.cs
@@ -15,31 +15,49 @@ namespace SoftCandy.Models
         [Display(Name = "CNPJ")]
         public string Cnpj { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(90, MinimumLength = 3, ErrorMessage = "O tamanho do {0} deve estar entre {2} e {1}.")]
         [Display(Name = "Razão Social")]
         public string RazaoSocial { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O tamanho do {0} deve estar entre {2} e {1}.")]
         [Display(Name = "Nome Fantasia")]
         public string NomeFantasia { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
         [Display(Name = "Celular")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "O tamanho do {0} deve ser 11 caracteres.")]
         public string CelularFornecedor { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [EmailAddress(ErrorMessage = "Entre um e-mail válido!")]
         [DataType(DataType.EmailAddress)]
         [Display(Name = "E-mail")]
         public string EmailFornecedor { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(60, MinimumLength = 4, ErrorMessage = "O tamanho do {0} deve estar entre {2} e {1}.")]
         [Display(Name = "Logradouro")]
         public string LogradouroFornecedor { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(6, MinimumLength = 1, ErrorMessage = "O tamanho do {0} deve estar entre {2} e {1}.")]
         [Display(Name = "Número")]
         public string NumeroFornecedor { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(60, MinimumLength = 4, ErrorMessage = "O tamanho do {0} deve estar entre {2} e {1}.")]
         [Display(Name = "Bairro")]
         public string BairroFornecedor { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(60, MinimumLength = 4, ErrorMessage = "O tamanho do {0} deve estar entre {2} e {1}.")]
         [Display(Name = "Cidade")]
         public string CidadeFornecedor { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(29, MinimumLength = 2, ErrorMessage = "O tamanho do {0} deve estar entre {2} e {1}.")]
         [Display(Name = "Estado")]
         public string EstadoFornecedor { get; set; }
 
